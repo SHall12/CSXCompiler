e53107b0be04523ca0ba65847aa21452358987e1
zend12 <shanehall2458@gmail.com>
2016-11-12 02:48:20 -0500

    Changed build file

--- Program 3/build.sh
+++ Program 3/build.sh
@@ -1,39 +1,15 @@
 #!/bin/bash
-### Instructions UNIX ###
 #### Generate CUP ####
-
+echo Building parser with csx.cup
 java -jar tools/java-cup-10l.jar csx.cup
 
 #### Compile ####
 
-echo Building scanner ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-java -jar ./tools/JFlex.jar csx.jflex 
-echo Building sym ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-javac -cp tools/java-cup-10l.jar:. sym.java
-echo Compiling scanner ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-javac -cp tools/java-cup-10l.jar:. Scanner.java
-echo Building ast ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-javac -cp tools/java-cup-10l.jar:. ast.java
-echo Building Yylex ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-javac -cp tools/java-cup-10l.jar:. Yylex.java
-echo Building parser ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-javac -cp tools/java-cup-10l.jar:. parser.java
-echo Building P3 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-javac -cp tools/java-cup-10l.jar:. P3.java
+echo Building scanner with csx.jflex
+java -jar ./tools/JFlex.jar csx.jflex
 
-#### Test ####
-
-#echo Testing ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#java -cp tools/java-cup-10l.jar:. P3 test1.csx
-
-
-
-#java -jar ./tools/java-cup-10l.jar csx.cup
-#javac sym.java
-#javac parser.java
-#createScanner
-#java -jar ./tools/JFlex.jar csx.jflex
-#javac Yylex.java
-
-#javac ast.java
-#javac P3.java
+for file in sym.java Scanner.java ast.java Yylex.java parser.java P3.java
+do
+    echo Compiling $file
+    javac -cp tools/java-cup-10l.jar:. $file
+done
