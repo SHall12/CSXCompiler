6543b661bf4c22860d65d39fe7fe3e7da1605df6
zend12 <shanehall2458@gmail.com>
2016-12-07 23:11:14 -0500

    Upload different version of code generator

---
+++ Program5/CodeGenerator2.java
@@ -0,0 +1,119 @@
+import java.util.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CodeGenerator {
+
+    private static ArrayList<Double> testScores = new ArrayList<Double>();
+    private static final String FILENAME = "simple.j";
+
+    public static void main(String[] args) {
+        BufferedWriter bw = null;
+		FileWriter fw = null;
+        Scanner sc = new Scanner(System.in);
+
+        // Create arraylist to store test scores
+        double score = 0;
+
+        // While input != -99
+        while (score != -99.0) {
+            System.out.print("Score: ");
+            score = sc.nextDouble();
+            testScores.add(score);
+        }
+
+        // Generate jasmin code
+        try {
+			String content = generateJasminProg();
+            fw = new FileWriter(FILENAME);
+			bw = new BufferedWriter(fw);
+			bw.write(content);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (bw != null)
+					bw.close();
+				if (fw != null)
+					fw.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+        }
+
+        // execute jasmin code
+        try {
+            Runtime.getRuntime().exec("java -jar jasmin.jar simple.j");
+            Runtime.getRuntime().exec("java simple > output.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String generateJasminProg() {
+        String prog = ".class simple\n.super java/lang/Object\n"
+            + ".method public <init>()V\n.limit stack 1\n.limit locals 1\n"
+            + "aload_0\ninvokespecial java/lang/Object/<init>()V\nreturn\n.end method\n\n"
+            + ".method public static main([Ljava/lang/String;)V\n"
+            + ".limit stack 4\n.limit locals 1\n"
+            + generateArray()
+            + "aload_0\ninvokestatic simple/computeMax([D)V\n"
+            + "aload_0\ninvokestatic simple/computeMin([D)V\nreturn\n.end method\n\n"
+            + generateFindingFunction(true)
+            + generateFindingFunction(false);
+        return prog;
+    }
+
+    private static String generateArray() {
+        String array = "";
+
+        if (testScores.size() < 6) {
+            array = "iconst_" + testScores.size() + "\n";
+        } else {
+            array = "bipush " + testScores.size() + "\n";
+        }
+
+        array += "newarray double\nastore_0\n";
+
+        for(int i = 0; i < testScores.size(); ++i) {
+            array += "aload_0\n";
+            if ( i < 6 ) {
+                array += "iconst_" + i + "\n";
+            } else {
+                array += "bipush " + i + "\n";
+            }
+            array += "ldc2_w " + testScores.get(i) + "\ndastore\n";
+        }
+
+        return array;
+    }
+
+    private static String generateFindingFunction(boolean isMax) {
+        String method = "";
+        if (isMax) {
+            method += ".method public static computeMax([D)V\n";
+        } else {
+            method += ".method public static computeMin([D)V\n";
+        }
+        method += ".limit stack 4\n.limit locals 6\n"
+                + "iconst_0\nistore_1\naload_0\niload_1\ndaload\ndstore_2\n"
+                + "aload_0\niload_1\ndaload\ndstore 4\n"
+                + "checkScore:\ndload 4\nldc2_w -99.\ndcmpg\n"
+                + "ifeq printScore\ndload 4\ndload_2\ndcmpg\n";
+        if (isMax) {
+            method += "iflt incrementIndex\ndload 4\ndstore_2\n";
+        } else {
+            method += "ifgt incrementIndex\ndload 4\ndstore_2\n";
+        }
+        method += "incrementIndex:\niload_1\niconst_1\niadd\nistore_1\n"
+                + "aload_0\niload_1\ndaload\ndstore 4\ngoto checkScore\n"
+                + "printScore:\ngetstatic java/lang/System/out Ljava/io/PrintStream;\n"
+                + "dload_2\ninvokevirtual java/io/PrintStream/println(D)V\n"
+                + "return\n.end method\n\n";
+
+        return method;
+    }
+}
